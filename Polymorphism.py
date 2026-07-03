c13fb7300ce7c58f328198efb6e28ee2bb317a2d
Muhammad Hammad Hussain <mhammadhussain81@gmail.com>
2026-07-03 10:13:57 +0500

    Add files via upload

---
+++ Polymorphism.py
@@ -0,0 +1,17 @@
+class dog:
+    def speak(self):
+        print("wOOOOO")
+class cat():
+    def speak(self):
+        print("MEOOOOOOOOWWW")
+
+for pet in[dog(),cat()]:
+           pet.speak()
+
+           text = "Python"
+           numbers = [1, 2, 3, 4]
+           t = (10, 20)
+
+           print(len(text))
+           print(len(numbers))
+           print(len(t))
